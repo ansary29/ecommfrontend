604b93c641961e121984cff0f71f9a4f3dfdfc9c
Mohamed Ansary <85570638+ansary29@users.noreply.github.com>
2023-07-27 21:48:27 +0530

    Add files via upload

---
+++ src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'mobile shop',
+  description: 'We sell a mobile & accessories',
+  keywords: 'electronics, buy electronics, cheap electroincs',
+}
+
+export default Meta
